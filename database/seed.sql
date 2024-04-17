466788a5279856512e1e3ec592e2b4f72986da12
Kenny <KennyYiu@gmail.com>
2024-04-17 17:32:55 +0800

    add users

--- database/seed.sql
+++ database/seed.sql
@@ -3,6 +3,7 @@ VALUES ('T-Shirts');
 INSERT INTO categories (name)
 VALUES('Pants');
 
+-------- products -----------
 INSERT INTO products (category_id,
                      name,
                      price,
@@ -35,3 +36,56 @@ VALUES (2, 'FFFF', 'M', 100);
 INSERT INTO product_options (product_id, hex_code, "size", stock)
 VALUES (2, 'FFFF', 'L', 100);
 
+-------- users ------------
+
+INSERT INTO users (
+    username,
+    password,
+    account_level_id,
+    created_at
+)
+VALUES (
+    'Alex38900',
+    '12345',
+    (SELECT id FROM account_levels WHERE level_name = 'super admin'),
+    CURRENT_TIMESTAMP
+);
+
+INSERT INTO users (
+    username,
+    password,
+    account_level_id,
+    created_at
+)
+VALUES (
+    'Peter9965',
+    '45678',
+    (SELECT id FROM account_levels WHERE level_name = 'customer'),
+    CURRENT_TIMESTAMP
+);
+
+INSERT INTO users (
+    username,
+    password,
+    account_level_id,
+    created_at
+)
+VALUES (
+    'ada3344',
+    '54321',
+    (SELECT id FROM account_levels WHERE level_name = 'customer'),
+    CURRENT_TIMESTAMP
+);
+
+INSERT INTO users (
+    username,
+    password,
+    account_level_id,
+    created_at
+)
+VALUES (
+    'bob1234',
+    '98765',
+    (SELECT id FROM account_levels WHERE level_name = 'admin'),
+    CURRENT_TIMESTAMP
+);
