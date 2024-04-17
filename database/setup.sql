1b124ae78213dc1f8f7b75bfbb9ff623c96cab35
Harvie <chengchifeng8@gmail.com>
2024-04-17 15:56:19 +0800

    updatedthedatabase

--- database/setup.sql
+++ database/setup.sql
@@ -2,12 +2,28 @@
 DROP DATABASE IF EXISTS groupproject1;
 CREATE DATABASE groupproject1;
 
+-- CREATE TABLE users (
+--  id SERIAL PRIMARY KEY,
+--  username varchar(20),
+--  password varchar(20),
+--  account_level enum,
+--  created_at timestamp WITH TIME ZONE DEFAULT now()
+-- );
+CREATE TABLE account_levels (
+    id SERIAL PRIMARY KEY,
+    level_name VARCHAR(20) UNIQUE
+);
+
+-- Insert the account levels into the lookup table
+INSERT INTO users (account_level_id)
+VALUES ('customer'), ('admin'), ('super admin');
+
 CREATE TABLE users (
- id SERIAL PRIMARY KEY,
- username varchar(20),
- password varchar(20),
- account_level enum,
- created_at timestamp WITH TIME ZONE DEFAULT now()
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(20),
+    password VARCHAR(20),
+    account_level_id INT,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
 );
 
 CREATE TABLE products (
@@ -21,7 +37,7 @@ uploaded_at timestamp WITH TIME ZONE DEFAULT now()
 CREATE TABLE product_image (
     id SERIAL PRIMARY KEY,
     product_id integer,
-    image string
+    image varchar(255)
 );
 
 CREATE TABLE product_options (
@@ -31,11 +47,27 @@ CREATE TABLE product_options (
     color_code varchar(50)
 );
 
+-- CREATE TABLE option_sizes (
+--     id SERIAL PRIMARY KEY,
+--     product_option_id integer,
+--     size enum,
+--     qunatity integer
+-- );
+
+CREATE TABLE sizes (
+    id SERIAL PRIMARY KEY,
+    size_name VARCHAR(2) UNIQUE
+);
+
+-- Insert the sizes into the lookup table
+INSERT INTO sizes (size_name)
+VALUES ('S'), ('M'), ('L'), ('XL');
+
 CREATE TABLE option_sizes (
     id SERIAL PRIMARY KEY,
-    product_option_id integer,
-    size enum,
-    qunatity integer
+    product_option_id INTEGER,
+    size_id INTEGER REFERENCES sizes (id),
+    quantity INTEGER
 );
 
 CREATE TABLE shopping_cart(
