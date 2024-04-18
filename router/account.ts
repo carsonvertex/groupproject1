ebf024067bf9cd48ce092a11f018170e58b381bc
Harvie <chengchifeng8@gmail.com>
2024-04-18 11:45:02 +0800

    accountUpdated

--- router/account.ts
+++ router/account.ts
@@ -1,25 +1,53 @@
-import { Router, Request, Response, NextFunction } from "express";
+import { Router, Request, Response } from "express";
 // import formidable from "formidable";
 import { pgClient } from "../pgClients";
-import expressSession from "express-session";
-import dotenv from "dotenv";
+import { checkPassword, hashPassword } from "../utils/hash";
 
-dotenv.config();
+
 
 export const accountRouter = Router();
 
 //get the existing products
 
 
-accountRouter.get("/checkAccount",register);
+accountRouter.post("/register", register);
 
 async function register(req: Request, res: Response) {
-    let UserQueryResult = (
-        await pgClient.query(
-            "SELECT Username FROM users"
-        )
-    ).rows;
-  console.log(UserQueryResult)
-    // res.json({ data: { Products: productQueryResult } });
-}
+    console.log("hi")
+    let { email, username, password } = req.body;
+    console.log(email, username, password)
+    let hashedPassword = await hashPassword(password)
+    console.log(email, username, password, hashedPassword)
+    try {
+        let userQueryResult = (
+            await pgClient.query(
+                "SELECT username,password,id FROM users WHERE email = $1 OR username = $2",
+                [email, username]
+            )
+        );
+        console.log(userQueryResult.rows)
 
+        userQueryResult = userQueryResult.rows[0]
+
+        //   email exists
+        if (userQueryResult) {
+            res.status(400).json({ message: "Duplicate entry." });
+            return;
+        }
+
+        const insertResult = await pgClient.query(
+            "INSERT INTO users (username, email, password,level) VALUES ($1, $2, $3,$4) RETURNING id",
+            [username, email, hashedPassword, "customer"]
+        );
+        console.log(insertResult);
+        const returningId = insertResult.rows[0].id;
+        res.json({
+            msg: "register successful",
+            userId: returningId,
+        });
+    } catch (e) {
+        console.log(e)
+        res.status(400).json({ message: e });
+
+    }
+}
--- utils/hash.ts
+++ utils/hash.ts
@@ -1,16 +1,16 @@
-import * as bcrypt from 'bcryptjs'
+import * as bcrypt from 'bcryptjs';
 
-const SALT_ROUNDS = 20
+const SALT_ROUNDS = 10
 
-export async function  hashPassword(plainPassword: string){
+export async function hashPassword(plainPassword: string) {
     const hash: string = await bcrypt.hash(plainPassword, SALT_ROUNDS)
     return hash;
 }
 
-export async function  checkPassword(option:{
+export async function checkPassword(option: {
     plainPassword: string
     hashedPassword: string
-}){
+}) {
     const isMatched: boolean = await bcrypt.compare(
         option.plainPassword,
         option.hashedPassword,
