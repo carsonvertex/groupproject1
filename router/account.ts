bc2f3799cc9c2c267cc8938bf450c6a0a839ecbb
Carson Lee <leelokhin0406@gmail.com>
2024-04-18 11:45:35 +0800

    uooo

--- router/account.ts
+++ router/account.ts
@@ -1,8 +1,10 @@
-import { Router, Request, Response, NextFunction } from "express";
+import { Router, Request, Response } from "express";
 // import formidable from "formidable";
 import { pgClient } from "../pgClients";
 import expressSession from "express-session";
 import dotenv from "dotenv";
+import { UserQueryType } from "../utils/type";
+import { hashPassword,checkPassword } from "../utils/hash";
 
 dotenv.config();
 
@@ -10,16 +12,42 @@ export const accountRouter = Router();
 
 //get the existing products
 
+accountRouter.post("/register",register)
 
-accountRouter.get("/checkAccount",register);
+accountRouter.post("/login",login)
+
 
 async function register(req: Request, res: Response) {
-    let UserQueryResult = (
+    let { email, username, password } = req.body;
+    let hashedPassword = await hashPassword(password)
+    try {
+      let userQueryResult: UserQueryType | undefined = (
         await pgClient.query(
-            "SELECT Username FROM users"
+          "SELECT username,password,id FROM users WHERE email = $1 OR username =$2", [email,username]
         )
-    ).rows;
-  console.log(UserQueryResult)
-    // res.json({ data: { Products: productQueryResult } });
-}
+      ).rows[0];
+   console.log(userQueryResult)
+      
+  
+      //   email exists
+      if (userQueryResult) {
+        res.status(400).json({ message: "Duplicate entry." });
+        return;
+      }
+  
+      const insertResult = await pgClient.query(
+        "inserT inTo users (username, email, password) Values ($1, $2, $3) returning id",
+        [username, email, hashedPassword]
+      );
+      console.log(insertResult);
+      const returningId = insertResult.rows[0].id;
+      res.json({
+        msg: "register successful",
+        userId: returningId,
+      });
+    } catch (e) {
+      res.status(400).json({ message: e });
+    }
+  }
 
+async function login(req:Request,res:Response){};
