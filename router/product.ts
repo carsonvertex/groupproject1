6e8182c060cce97fbc501dda4f892c41c344f32e
Harvie <chengchifeng8@gmail.com>
2024-04-23 13:37:15 +0800

    updated login Page with Username in HomePage

--- router/product.ts
+++ router/product.ts
@@ -6,9 +6,9 @@ import formidable from "formidable";
 export const productRouter = Router();
 
 productRouter.get("/showProduct", showProduct);
-productRouter.post("/newProduct", newProduct);
-productRouter.put("/editProduct", editProduct);
-productRouter.delete("/delProduct", delProduct);
+// productRouter.post("/newProduct", newProduct);
+// productRouter.put("/editProduct", editProduct);
+// productRouter.delete("/delProduct", delProduct);
 
 async function showProduct(req: Request, res: Response) {
     let productQueryResult = (
@@ -21,76 +21,76 @@ async function showProduct(req: Request, res: Response) {
 
 }
 
-async function newProduct(req: Request, res: Response) {
-    const form = formidable({
-        uploadDir: __dirname + "/../uploads",
-        keepExtensions: true,
-        minFileSize: 0,
-        allowEmptyFiles: true,
-      });
-      let name:string;
-      let price:number;
-      let description:string;
+// async function newProduct(req: Request, res: Response) {
+//     const form = formidable({
+//         uploadDir: __dirname + "/../uploads",
+//         keepExtensions: true,
+//         minFileSize: 0,
+//         allowEmptyFiles: true,
+//       });
+//       let name:string;
+//       let price:number;
+//       let description:string;
 
 
-      form.parse(req, async (err, fields, files) => {
-      if (err) {
-        console.log(err);
-        res.status(500).json({ message: "Internal server erorr!" });
-      }
+//       form.parse(req, async (err, fields, files) => {
+//       if (err) {
+//         console.log(err);
+//         res.status(500).json({ message: "Internal server erorr!" });
+//       }
   
-      if (fields.name) {
-        name = fields.name![0];
-      }
-      if (fields.price) {
-        price = fields.price![0];
-      }
+//       if (fields.name) {
+//         name = fields.name![0];
+//       }
+//       if (fields.price) {
+//         price = fields.price![0];
+//       }
   
-      if (files.photo) {
-        memoImage = files.photo[0].newFilename;
-      }
+//       if (files.photo) {
+//         memoImage = files.photo[0].newFilename;
+//       }
 
-      let productInsertResult = (await pgClient.query(
-        "INSERT INTO products (name,price,description) VALUES ($1,$2, $3) RETURNING id",
-        [name,price,description]
-      ))
+//       let productInsertResult = (await pgClient.query(
+//         "INSERT INTO products (name,price,description) VALUES ($1,$2, $3) RETURNING id",
+//         [name,price,description]
+//       ))
   
-      res.json({
-        data: {
-          id: productInsertResult.rows[0].id,
-          photo: productInsertResult.rows[0].image,
-        },
-      });
-    });
-  }
+//       res.json({
+//         data: {
+//           id: productInsertResult.rows[0].id,
+//           photo: productInsertResult.rows[0].image,
+//         },
+//       });
+//     });
+//   }
 
-async function editProduct(req: Request, res: Response) {
-    let { name } = req.body;
-    let {id}= req.query;
-    let productUpdateResult = await pgClient.query(
-        "UPDATE categories SET name=$1 WHERE id = $2 RETURNING *",
-        [name,id]
-    );
+// async function editProduct(req: Request, res: Response) {
+//     let { name } = req.body;
+//     let {id}= req.query;
+//     let productUpdateResult = await pgClient.query(
+//         "UPDATE categories SET name=$1 WHERE id = $2 RETURNING *",
+//         [name,id]
+//     );
 
-    if (productUpdateResult.rowCount == 1) {
-        res.json({
-            message: "update success",
-        });
-    }}
+//     if (productUpdateResult.rowCount == 1) {
+//         res.json({
+//             message: "update success",
+//         });
+//     }}
 
-async function delProduct(req: Request, res: Response) {
+// async function delProduct(req: Request, res: Response) {
 
-    let targetId = parseInt(req.query.id as string);
+//     let targetId = parseInt(req.query.id as string);
 
-    let productDeleteResult = await pgClient.query(
-        "DELETE FROM categories WHERE id =$1",
-        [targetId]
-    );
+//     let productDeleteResult = await pgClient.query(
+//         "DELETE FROM categories WHERE id =$1",
+//         [targetId]
+//     );
 
-    if (productDeleteResult.rowCount == 1) {
-        res.json({ message: "Delete category successful" });
-    } else {
-        res.status(400).json({ message: "Delete category failed" });
-    }
+//     if (productDeleteResult.rowCount == 1) {
+//         res.json({ message: "Delete category successful" });
+//     } else {
+//         res.status(400).json({ message: "Delete category failed" });
+//     }
 
-}
+// }
